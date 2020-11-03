96e97e526b31022bdb8093571151093559135233
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2020-11-03 17:32:18 -0500

    Fix some formatting and bump to 1.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
 
 setup(
     name             = 'pfdorun',
-    version          = '1.0.0',
+    version          = '1.0.2',
     description      = 'The pf-pfdorun plugin is a general purpose "swiss army" knife DS plugin that can be used to execute some CLI type commands on input directory trees.',
     long_description = readme,
     author           = 'Rudolph Pienaar',
